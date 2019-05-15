aaea947c0e4ec6ef06a60088edd82f80c3cab22a
Olivier JM <manolivier93@gmail.com>
2019-05-15 14:07:57 +0200

    checking for course creation and authentication

--- __tests__/course.spec.js
+++ __tests__/course.spec.js
@@ -22,4 +22,58 @@ describe("courses resolvers", () => {
     expect(data.getCourses).toBe(null);
     expect(errors[0].message).toBe("you must be logged in");
   });
+
+  test("should create a course and return proper data", async () => {
+    const course = await axios.post(process.env.URL, {
+      query: `
+          mutation {
+            addCourse(name: "Introduction", createdBy:"olivier") {
+              name
+              createdAt
+              createdBy
+            }
+          }
+      `
+    });
+    const {
+      data: {
+        data: { addCourse }
+      }
+    } = await course;
+    expect(addCourse.name).toBe("Introduction");
+    expect(addCourse.createdBy).toBe("olivier");
+    expect(addCourse.createdAt).toBe(null);
+  });
+  test("should query all courses", async () => {
+    const response = await axios.post(
+      process.env.URL,
+      {
+        query: `
+          query {
+              getCourses {
+                  name
+              }
+          }
+          `
+      },
+      {
+        headers: {
+          authorization: process.env.TOKEN
+        }
+      }
+    );
+    const { data } = response;
+
+    expect(data).toMatchObject({
+      data: {
+        getCourses: [
+          {
+            name: "Introduction"
+          }
+        ]
+      }
+    });
+    expect(data.data).toHaveProperty("getCourses");
+    expect(data.data.getCourses[0].name).toBe("Introduction");
+  });
 });
