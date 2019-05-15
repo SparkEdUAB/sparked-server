ed7368f71e9137050dfc557a57d5f9f6d1a3688d
Olivier JM <manolivier93@gmail.com>
2019-05-15 07:47:21 +0200

    properly configured babel with jest

--- __tests__/user.spec.js
+++ __tests__/user.spec.js
@@ -1,9 +1,9 @@
 import axios from "axios";
 
-// describe("user resolvers", () => {
-test("should query all users", async () => {
-  const response = await axios.post("http://localhost:3000/graphql", {
-    query: `
+describe("user resolvers", () => {
+  test("should query all users", async () => {
+    const response = await axios.post("http://localhost:3000/graphiql", {
+      query: `
         query {
             getUnits {
                 name
@@ -11,33 +11,33 @@ test("should query all users", async () => {
             }
         }
         `
-  });
-  const { data } = response;
-  expect(data).toMatchObject({
-    data: {
-      getUnits: [
-        {
-          name: "Intro",
-          _id: "5cd57e27678b6614d053e7ec"
-        },
-        {
-          name: "Second Intro",
-          _id: "5cd58e15ed50241878430490"
-        },
-        {
-          name: "Second Intro",
-          _id: "5cd7df4e3307401e57700a77"
-        },
-        {
-          name: "Second Intro",
-          _id: "5cd7df4f3307401e57700a79"
-        },
-        {
-          name: "Second Intro",
-          _id: "5cd7df503307401e57700a7b"
-        }
-      ]
-    }
+    });
+    const { data } = response;
+    expect(data).toMatchObject({
+      data: {
+        getUnits: [
+          {
+            name: "Intro",
+            _id: "5cd57e27678b6614d053e7ec"
+          },
+          {
+            name: "Second Intro",
+            _id: "5cd58e15ed50241878430490"
+          },
+          {
+            name: "Second Intro",
+            _id: "5cd7df4e3307401e57700a77"
+          },
+          {
+            name: "Second Intro",
+            _id: "5cd7df4f3307401e57700a79"
+          },
+          {
+            name: "Second Intro",
+            _id: "5cd7df503307401e57700a7b"
+          }
+        ]
+      }
+    });
   });
 });
-// });
