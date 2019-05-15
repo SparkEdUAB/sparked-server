a7c54ac567c072cda6cc1b3e72ae3acd4280c308
Olivier JM <manolivier93@gmail.com>
2019-05-15 13:26:39 +0200

    added user tests

---
+++ __tests__/user.spec.js
@@ -0,0 +1,57 @@
+import axios from "axios";
+import dotenv from "dotenv";
+
+dotenv.config();
+
+describe("user resolvers", () => {
+  test("should create the user", async () => {
+    const registerResponse = await axios.post(process.env.URL, {
+      query: `
+            mutation {
+                register(email:"joe@gmail.com", password:"123456"){
+                    username
+                    email
+                    password
+                }
+                }
+        `
+    });
+
+    const {
+      data: {
+        data: { register }
+      }
+    } = registerResponse;
+    expect(register.username).toBeNull();
+    expect(register.email).toBe("joe@gmail.com");
+  });
+  test("user should be able to login", async () => {
+    const loginResponse = await axios.post(process.env.URL, {
+      query: `
+            mutation {
+                login(email:"joe@gmail.com", password:"123456")
+                }
+          `
+    });
+    const {
+      data: {
+        data: { login }
+      }
+    } = loginResponse;
+    expect(login).toBeTruthy();
+  });
+  test("shouldn't login when password is wrong", async () => {
+    const response = await axios.post(process.env.URL, {
+      query: `
+            mutation {
+                login(email:"joe@gmail.com", password:"3456")
+                }
+          `
+    });
+    const {
+      data: { data, errors }
+    } = response;
+    expect(data).toBeNull();
+    expect(errors[0].message).toBe("Incorrect password ");
+  });
+});
