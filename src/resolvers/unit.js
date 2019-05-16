58c3c3b87e8e0341e8377d9f43df3daa38630403
Olivier JM <manolivier93@gmail.com>
2019-05-16 16:48:38 +0200

    added createdBy and the courseId to units

--- src/resolvers/unit.js
+++ src/resolvers/unit.js
@@ -7,15 +7,17 @@ const unitResolvers = {
       if (!user) {
         throw new AuthenticationError('you must be logged in')
       }
-      return Unit.find({})
+      return Unit.find({ createdBy: user._id })
     },
   },
   Mutation: {
-    addUnit(root, args) {
+    addUnit(root, args, { user }) {
       let unit = new Unit()
       unit.name = args.name
+      unit.courseId = args.courseId
       unit.createdAt = args.createdAt
-      unit.createdBy = args.createdBy
+      unit.createdBy = user._id
+      // unit.createdA
       return unit.save()
     },
   },
