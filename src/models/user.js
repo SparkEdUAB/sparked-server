7355b967d40316213957248d0edb37b0cb2217f2
Olivier JM <manolivier93@gmail.com>
2019-06-07 10:34:56 +0200

    added user details

--- src/models/user.js
+++ src/models/user.js
@@ -1,10 +1,14 @@
 import mongoose from 'mongoose'
 
 const userSchema = mongoose.Schema({
+  name: String,
+  gender: String,
+  roles: [String],
   username: String,
   email: String,
   password: String,
   createAt: Date,
+  status: Boolean,
 })
 
 const User = mongoose.model('User', userSchema)
