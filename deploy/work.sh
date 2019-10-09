71c0e369001d9c4315d1e29793fbd321cfa2a4cd
Olivier <olivier@thezig.io>
2019-10-09 19:33:07 +0200

    added the work deploy script

---
+++ deploy/work.sh
@@ -0,0 +1,32 @@
+#!/bin/bash
+set -e
+
+### Configuration ###
+
+APP_DIR=/var/www/spark
+GIT_URL=git://github.com/sparkeduab/sparked-server
+RESTART_ARGS=
+
+# Uncomment and modify the following if you installed Passenger from tarball
+#export PATH=/path-to-passenger/bin:$PATH
+
+
+### Automation steps ###
+
+set -x
+
+# Pull latest code
+if [[ -e $APP_DIR/code ]]; then
+  cd $APP_DIR/code
+  git pull
+else
+  git clone $GIT_URL $APP_DIR/code
+  cd $APP_DIR/code
+fi
+
+# Install dependencies
+npm install --production
+npm prune --production
+
+# Restart app
+passenger-config restart-app --ignore-app-not-running --ignore-passenger-not-running $RESTART_ARGS $APP_DIR/code
