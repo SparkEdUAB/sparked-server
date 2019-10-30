70f6a8035882872f4d383cf69fadcf14790044ed
OlivierJM <manolivier93@gmail.com>
2019-10-30 22:49:26 +0200

    added feedback model

---
+++ src/resolvers/notifications.js
@@ -0,0 +1,9 @@
+// notifications should be added on the following grounds
+// - a new course is created
+// - a new unit is created
+// - a new topic is created
+// - new files are uploaded
+// - any new changes should be added too
+// - all registered users should see get the notification
+// - notifications should only last for 30days
+//
