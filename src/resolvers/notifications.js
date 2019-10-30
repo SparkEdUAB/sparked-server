9837a34e4a0e6fdc4f0402c4389ef50f7bc7d264
OlivierJM <manolivier93@gmail.com>
2019-10-30 23:16:02 +0200

    added some guidelines on new collections

--- src/resolvers/notifications.js
+++ src/resolvers/notifications.js
@@ -6,4 +6,5 @@
 // - any new changes should be added too
 // - all registered users should see get the notification
 // - notifications should only last for 30days
-//
+// - notifications should link to their specific contents
+//      - if a new course was created, then i should be able to click on it
