9cd0dff6a3716b231dae8b8046f0529e1a9ff20e
OlivierJM <manolivier93@gmail.com>
2019-10-30 22:26:34 +0200

    add notification model

--- src/resolvers/resource.js
+++ src/resolvers/resource.js
@@ -14,6 +14,7 @@ export const resourceResolver = {
       return Resource.find({ topicId: args.topicId })
     },
     getResource(root, args, context) {
+    
       return Resource.findOne({ _id: Mongoose.Types.ObjectId(args.id) })
     },
   },
